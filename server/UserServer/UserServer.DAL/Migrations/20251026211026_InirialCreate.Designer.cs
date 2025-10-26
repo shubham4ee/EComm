1ecb945f826f6362f48ccef7a0b90b91f9b9dd49
shubham4ee <shubham4ee@gmail.com>
2025-10-26 14:32:54 +0530

    Migration Added

--- server/UserServer/UserServer.DAL/Migrations/20251026211026_InirialCreate.Designer.cs
+++ server/UserServer/UserServer.DAL/Migrations/20251026211026_InirialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UserServer.DAL.DataContext;
@@ -11,9 +12,11 @@ using UserServer.DAL.DataContext;
 namespace UserServer.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251026211026_InirialCreate")]
+    partial class InirialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
